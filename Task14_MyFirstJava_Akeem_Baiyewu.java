95610adee4255b32c7db0c3e416328a5c7eaf4fa
abaiyewu <akeembaiyewu@gmail.com>
2022-11-28 10:17:36 +0100

    My First Java Project - Task 14

---
+++ Task14_MyFirstJava_Akeem_Baiyewu.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world. Java is looking interesting");
+
+        System.out.println("The name is Akeem Baiyewu and I will like to say, Welcome to my Java World");
+        System.out.println("Switching to the World of Software Testing is proving to be a very wise decision on my part");
+        System.out.println("That being said, Let me Re-Introduce Myself in Software Testing Terms:");
+        System.out.println("Am A well trained Software Tester with experience in Manual & Automation with experience in the Agile Environment Testing using various tools like:");
+        System.out.println("Google Doc");
+        System.out.println("Google Sheet");
+        System.out.println("Jira");
+        System.out.println("Postman API");
+        System.out.println("Newman");
+        System.out.println("Selenium, ...");
+        System.out.println("I also have other Soft skills like:");
+        System.out.println("A very Fast & Smart Learner, good in critical thinking, Team Player, Leadership Skill, communication skills in abundance");
+        System.out.println("I will end this by saying; Even a broken watch is correct twice a day. Everyone has a purpose/passion in life. All it takes is to identify and stick to it. That's Software Testing for me. We Move!!");
+    }
+}
